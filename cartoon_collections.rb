68e21a45012b552c1422f1c501088a796e83e343
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 11:31:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names) # code an argument here
   # Your code here
+  names.each do |name|
+    puts name
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.collect do |call|
+    call.capitalize!
+    call << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
