2fff6689d67ab3f1b562c727339991762e63de8a
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 11:35:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(names) # code an argument here
   end
 end
 
-def summon_captain_planet(calls)# code an argument here
+def summon_captain_planet(calls) # code an argument here
   # Your code here
   calls.collect do |call|
     call.capitalize!
@@ -13,11 +13,21 @@ def summon_captain_planet(calls)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |type|
+    if strings.find(type)
+      return type
+    end
+  end
+
+  return nil
 end
